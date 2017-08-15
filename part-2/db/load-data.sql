bf0be631fd8822c1e9aee220898aaef3c8a80536
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-08-15 13:22:33 -0700

    wip save, getting pkey error

--- part-2/db/load-data.sql
+++ part-2/db/load-data.sql
@@ -1,5 +1,5 @@
-COPY guests(id, name, email) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/guests.csv'
+COPY guests(id, name, email) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/guests.csv' DELIMITER ',' CSV HEADER;
 
-COPY rooms(id, name, capacity) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/rooms.csv'
+COPY rooms(id, room_number, capacity) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/rooms.csv' DELIMITER ',' CSV HEADER;
 
-COPY bookings(id, room_id, guest_id, check_in, check_out) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/bookings.csv'
+COPY bookings(id, room_id, guest_id, check_in, check_out) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/bookings.csv' DELIMITER ',' CSV HEADER;
--- part-2/db/schema.sql
+++ part-2/db/schema.sql
@@ -1,25 +1,22 @@
-CREATE TABLE "guests" (
-"id"  SERIAL NOT NULL ,
-"name" VARCHAR(255) NOT NULL ,
-"email" VARCHAR(255) ,
-PRIMARY KEY ("id")
+CREATE TABLE guests (
+id  SERIAL PRIMARY KEY  ,
+name VARCHAR(255) NOT NULL ,
+email VARCHAR(255)
 );
 
-CREATE TABLE "rooms" (
-"id"  SERIAL ,
-"room_number" INTEGER ,
-"capacity" INTEGER NOT NULL ,
-PRIMARY KEY ("id")
+CREATE TABLE rooms (
+id  SERIAL PRIMARY KEY ,
+room_number VARCHAR(255) NOT NULL,
+capacity INTEGER NOT NULL
 );
 
-CREATE TABLE "bookings" (
-"id"  SERIAL ,
-"guest_id" VARCHAR(255) NOT NULL ,
-"room_number_id" INTEGER NOT NULL ,
-"check_out" DATE NOT NULL ,
-"check_in" DATE NOT NULL ,
-PRIMARY KEY ("id")
+CREATE TABLE bookings (
+id  SERIAL PRIMARY KEY ,
+guest_id VARCHAR(255) NOT NULL ,
+room_number_id VARCHAR(255) NOT NULL ,
+check_out DATE NOT NULL ,
+check_in DATE NOT NULL
 );
 
-ALTER TABLE "bookings" ADD FOREIGN KEY ("guest_id") REFERENCES "guests" ("id");
-ALTER TABLE "bookings" ADD FOREIGN KEY ("room_number_id") REFERENCES "rooms" ("id");
+ALTER TABLE bookings ADD FOREIGN KEY (guest_id) REFERENCES guests (id);
+ALTER TABLE bookings ADD FOREIGN KEY (room_number_id) REFERENCES rooms (id);
