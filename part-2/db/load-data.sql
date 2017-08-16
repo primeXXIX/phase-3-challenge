6ff4dfb0fa40a0e0d45a0a2b7c62f22124e82538
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-08-15 20:43:51 -0700

    fixes db

--- part-2/db/load-data.sql
+++ part-2/db/load-data.sql
@@ -1,5 +1,5 @@
-COPY guests(id, name, email) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/guests.csv' DELIMITER ',' CSV HEADER;
+\COPY guests(id,name,email) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/guests.csv' DELIMITER ',' CSV HEADER;
 
-COPY rooms(id, room_number, capacity) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/rooms.csv' DELIMITER ',' CSV HEADER;
+\COPY rooms(id,room_number,capacity) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/rooms.csv' DELIMITER ',' CSV HEADER;
 
-COPY bookings(id, room_id, guest_id, check_in, check_out) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/bookings.csv' DELIMITER ',' CSV HEADER;
+\COPY bookings(id,room_id,guest_id,check_in,check_out) from '/Users/aaronboatwright/Desktop/LG/phase-3-challenge/part-2/db/bookings.csv' DELIMITER ',' CSV HEADER;
